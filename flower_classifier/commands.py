207fcf3ffc960849e72a3a98d2f99b8b776fc674
Alexander Semenyuk <aww.semenyuk@gmail.com>
2025-12-29 22:35:20 +0300

    revert cli

--- flower_classifier/commands.py
+++ flower_classifier/commands.py
@@ -2,33 +2,13 @@
 
 import fire
 
-
-def download_data():
-    from flower_classifier.download_data import main  # noqa: PLC0415
-
-    return main()
-
-
-def preprocess_data():
-    from flower_classifier.preprocess_data import main  # noqa: PLC0415
-
-    return main()
-
-
-def train():
-    from flower_classifier.train import main  # noqa: PLC0415
-
-    return main()
+from flower_classifier.download_data import main as download_data
+from flower_classifier.preprocess_data import main as preprocess_data
+from flower_classifier.train import main as train
 
 
 def main():
-    fire.Fire(
-        {
-            "download_data": download_data,
-            "preprocess_data": preprocess_data,
-            "train": train,
-        }
-    )
+    fire.Fire({"download_data": download_data, "preprocess_data": preprocess_data, "train": train})
 
 
 if __name__ == "__main__":
